f7d469170ccbb773eb7ad691020859ea9cd4a507
Mathias Chevez <davidchevez90@gmail.com>
2024-06-29 17:31:04 -0600

    connect postgres database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import { db } from "~/server/db";
 
 const MOCK_IMAGE_URLS = [
   'https://utfs.io/f/63559bb6-ad80-4180-81f2-6e9ccdd2734f-mhbjtf.jpg',
@@ -12,12 +13,17 @@ const mockImages = MOCK_IMAGE_URLS.map((url, index) => ({
   url,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+
+  console.log(posts)
+
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {[...mockImages, ...mockImages, ...mockImages, ...mockImages].map(image => (
-          <div key={image.id} className="w-48">
+        {posts.map(post => (<div key={post.id}>{post.name}</div>))}
+        {[...mockImages, ...mockImages, ...mockImages, ...mockImages].map((image, i) => (
+          <div key={i} className="w-48">
             <img src={image.url} alt={`Image ${image.id}`} />
           </div>
         ))}
