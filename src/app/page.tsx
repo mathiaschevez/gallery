21c5faab0372a83f64c7fabc52b10d776bbc0cca
Mathias Chevez <davidchevez90@gmail.com>
2024-07-09 21:42:50 -0600

    auth for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,32 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+async function Images() {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id),
   });
 
   return (
+    <div className="flex flex-wrap gap-4">
+      {[...images, ...images, ...images, ...images].map((image, i) => (
+        <div key={i} className="w-48 flex flex-col">
+          <img src={image.url} alt={`Image ${image.id}`} />
+          <div>{image.name}</div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default async function HomePage() {
+  return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
-        {[...images, ...images, ...images, ...images].map((image, i) => (
-          <div key={i} className="w-48 flex flex-col">
-            <img src={image.url} alt={`Image ${image.id}`} />
-            <div>{image.name}</div>
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div className="w-full h-full text-center text-2xl">Please sign in to view images</div>
+      </SignedOut>
+      <SignedIn><Images /></SignedIn>
     </main>
   );
 }
