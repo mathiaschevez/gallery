964ef4404aa584edba87cf49b322e43686977d70
Mathias Chevez <davidchevez90@gmail.com>
2024-07-10 16:18:42 -0600

    only query users images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
-import { db } from "~/server/db";
+import { getImages } from "~/server/queries";
 
 export const dynamic = "force-dynamic";
 
 async function Images() {
-  const images = await db.query.images.findMany({
-    orderBy: (model, { desc }) => desc(model.id),
-  });
+  const images = await getImages();
 
   return (
     <div className="flex flex-wrap gap-4">
@@ -17,7 +15,7 @@ async function Images() {
         </div>
       ))}
     </div>
-  )
+  );
 }
 
 export default async function HomePage() {
