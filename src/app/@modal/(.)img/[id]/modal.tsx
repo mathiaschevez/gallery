8031dd468e8074b0cd7f80d34c95e95a8fdf48df
Mathias Chevez <davidchevez90@gmail.com>
2024-07-24 13:21:18 -0600

    update image modal

--- src/app/@modal/(.)img/[id]/modal.tsx
+++ src/app/@modal/(.)img/[id]/modal.tsx
@@ -21,11 +21,11 @@ export function Modal({ children }: { children: React.ReactNode }) {
   return createPortal(
     <dialog
       ref={dialogRef}
-      className="w-screen h-screen bg-zinc-900/50 m-0"
+      className="w-screen h-screen bg-zinc-900/50 m-0 text-white"
       onClose={onDismiss}
     >
       {children}
-      <button onClick={onDismiss} className="close-button" />
+      {/* <button onClick={onDismiss} className="close-button" /> */}
     </dialog>,
     document.getElementById('modal-root')!
   );
