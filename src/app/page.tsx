59b0103be6e3b98f3640cf765dfb39d8f4660105
Mathias Chevez <davidchevez90@gmail.com>
2024-07-28 19:39:38 -0600

    add image card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,20 +10,25 @@ async function Images() {
 
   return (
     <div className="flex flex-wrap justify-center gap-4 p-4">
-      {images.map(image => (
-        <div key={image.id} className="flex flex-col w-48 h-48">
-          <Link href={`/img/${image.id}`}>
-            <Image
-              src={image.url}
-              alt={image.name}
-              width={192}
-              height={192}
-              style={{ objectFit: 'contain' }}
-            />
-          </Link>
-          <div>{image.name}</div>
-        </div>
-      ))}
+      {images.map(image => <ImageCard key={image.id} image={image} />)}
+    </div>
+  );
+}
+
+function ImageCard({ image }: { image: { id: number; url: string; name: string } }) {
+  return (
+    <div className="bg-slate-800 border border-white flex flex-col w-72 h-80">
+      <Link href={`/img/${image.id}`} className="flex flex-col items-center justify-center h-full p-3">
+        <Image
+          className="max-h-60"
+          src={image.url}
+          alt={image.name}
+          width={192}
+          height={192}
+          style={{ objectFit: 'contain' }}
+        />
+        <div className="mt-auto w-full">{image.name}</div>
+      </Link>
     </div>
   );
 }
